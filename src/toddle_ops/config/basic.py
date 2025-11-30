fe6cef590cfbf11d52d38bd14e37f7f2b19795c8
asenetcky <me@asenetcky.dev>
2025-11-30 08:57:32 -0500

    feat: add event compaction config

--- src/toddle_ops/config/basic.py
+++ src/toddle_ops/config/basic.py
@@ -1,8 +1,14 @@
 from google.genai import types
+from google.adk.apps.app import EventsCompactionConfig
 
 retry_config = types.HttpRetryOptions(
     attempts=4,  # Maximum retry attempts
     exp_base=7,  # Delay multiplier
     initial_delay=1,
     http_status_codes=[429, 500, 503, 504],  # Retry on these HTTP errors
+)
+
+events_compaction_config = EventsCompactionConfig(
+    compaction_interval=3,
+    overlap_size=1,
 )
