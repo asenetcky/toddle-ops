c468626a9ec17cfad6de36269b421b587f455c11
asenetcky <me@asenetcky.dev>
2025-11-30 08:58:00 -0500

    feat: add sessions service

---
+++ src/toddle_ops/services/sessions.py
@@ -0,0 +1,9 @@
+from google.adk.sessions import DatabaseSessionService
+#from google.adk.sessions import InMemorySessionService
+from pathlib import Path
+
+database_path = Path.cwd() / "data"
+
+# basics sessions service
+db_url = f"sqlite+aiosqlite:///{database_path.resolve()}/toddle_ops_sessions.db"  # Local SQLite file
+session_service = DatabaseSessionService(db_url=db_url)
