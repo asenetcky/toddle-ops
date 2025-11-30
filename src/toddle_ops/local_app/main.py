1c11e9ba07ba73ef841c77d0d4f431cb39c9dc91
asenetcky <me@asenetcky.dev>
2025-11-30 15:47:35 -0500

    fix: module path in main

--- src/toddle_ops/local_app/main.py
+++ src/toddle_ops/local_app/main.py
@@ -5,23 +5,20 @@ from google.adk.runners import Runner
 
 from toddle_ops.services.sessions import session_service
 from toddle_ops.services.memory import memory_service
-from toddle_ops.app.agent import adk_app
+from toddle_ops.local_app.agent import adk_app
 
 load_dotenv()
 
 logging.basicConfig(
     filename="logger.log",
-    level=logging.INFO,
-    # format="%(filename)s:%(lineno)s %(levelname)s:%(message)s",
+    level=logging.WARNING,
     format="'%(asctime)s - %(levelname)s - %(name)s - %(message)s'",
 )
 
 APP_NAME = "toddle_ops"
 
 runner = Runner(
-    # agent=root_agent,
     app=adk_app,
-    # app_name=APP_NAME,
     session_service=session_service,
     memory_service=memory_service,
 )
@@ -30,7 +27,7 @@ runner = Runner(
 async def main():
     try:
         _response = await runner.run_debug(
-            "Hello - I would like a toddler project please."
+             "Please provide a project."
         )
     except Exception as e:
         print(f"An error occurred during toddle ops execution: {e}")
