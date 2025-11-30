10530d636fe8020fe74a55eefd51ea84dff30ae2
asenetcky <me@asenetcky.dev>
2025-11-30 13:47:29 -0500

    feat: setup vertex

--- src/toddle_ops/app/agent.py
+++ src/toddle_ops/app/agent.py
@@ -6,11 +6,15 @@ from google.adk.apps.app import App
 from toddle_ops.agents.root_agent.agent import root_agent
 from toddle_ops.config.basic import events_compaction_config
 from google.adk.plugins.logging_plugin import LoggingPlugin
+import vertexai
 
 # Load environment variables from .env
 try:
     load_dotenv()
-    GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+    vertexai.init(
+        project=os.environ["GOOGLE_CLOUD_PROJECT"],
+        location=os.environ["GOOGLE_CLOUD_LOCATION"],
+    )
 except Exception as e:
     print(f"Auth error: Details: {e}")
 
