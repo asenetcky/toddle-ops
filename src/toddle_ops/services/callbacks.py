f62514481e1f06360f4471cb6f77ba528b270617
asenetcky <me@asenetcky.dev>
2025-11-30 11:18:30 -0500

    feat: add callbacks service

---
+++ src/toddle_ops/services/callbacks.py
@@ -0,0 +1,7 @@
+# Save Session to memory automatically
+# intended for use at the end of a conversation - batch processing/ reduce api calls
+async def auto_save_to_memory(callback_context):
+    """Automatically save session to memory after each agent turn."""
+    await callback_context._invocation_context.memory_service.add_session_to_memory(
+        callback_context._invocation_context.session
+    )
