a727d6c76a32001d8741087ef19dba5ed1593cc5
asenetcky <me@asenetcky.dev>
2025-12-08 20:06:13 -0500

    feat: toying with parsing projects

--- notebooks/database-mvp.py
+++ notebooks/database-mvp.py
@@ -19,12 +19,12 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import marimo as mo
-    from google.adk.agents import LlmAgent
+    from google.adk.agents import LlmAgent, ParallelAgent
     from google.adk.models.google_llm import Gemini
     from google.adk.models.lite_llm import LiteLlm
     from google.adk.runners import InMemoryRunner
     from google.adk.sessions import InMemorySessionService
-    return InMemoryRunner, LiteLlm, LlmAgent
+    return InMemoryRunner, LiteLlm, LlmAgent, ParallelAgent, mo
 
 
 @app.cell
@@ -43,7 +43,7 @@ def _():
         duration_minutes: int = Field(
             description="The estimated duration of the project in minutes."
         )
-        instructions: str = Field(description="A list of instructions for the project.")
+        instructions: str = Field(description="Step by step instructions for the project.")
 
 
     class Project(ProjectBase, SQLModel, table=True):
@@ -74,24 +74,146 @@ def _():
 def _(LiteLlm, LlmAgent, ProjectPackage):
     project_conversion_agent = LlmAgent(
         name="project_conversion_agent",
-        model=LiteLlm(model="ollama_chat/gemma3:4b"),
+        #model=Gemini(model="gemini-2.5-flash-lite"),
+        model=LiteLlm(model="ollama_chat/deepseek-r1:32b"),
         instruction="""You are adept at converting the
         provided to you into Project and Material Classes.""",
         output_schema=ProjectPackage,
-        output_key="project_dict",
     )
-    return (project_conversion_agent,)
+    return
 
 
 @app.cell
-def _(InMemoryRunner, project_conversion_agent):
-    runner = InMemoryRunner(agent=project_conversion_agent)
-    return (runner,)
+def _():
+    return
 
 
 @app.cell
-async def _(runner):
-    await runner.run_debug()
+def _():
+    # _runner = InMemoryRunner(agent=project_conversion_agent)
+    # result = await _runner.run_debug(
+    #     """
+    #     You are given this this example project:
+
+    #     Name: Toddler Art Project
+    #     description: A simple art project for toddlers using basic materials.
+    #     duration (in minutes): 30
+    #     instructions: 1. Gather all materials. 2. Provide paper and crayons to the toddler. 
+    #         3. Encourage the toddler to draw freely. 4. Display the artwork proudly.
+    #     materials:
+    #        2 Sheets of paper, crayons (assorted colors), glue stick, safety scissors. 
+
+    #     Convert this information into the ProjectBase and MaterialBase classes, and 
+    #     return a ProjectPackage object containing the project and its materials.
+
+    #     """
+    # )
+    return
+
+
+@app.cell
+def _():
+    # result
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md("""
+    Okay I'm finding out that one big agent isn't working out too great. gemini-2.5-flash-lite works incredibly well - but I dont wanna pay if I can help it. Lets try parallel smaller agents
+    """)
+    return
+
+
+@app.cell
+def _(LiteLlm, LlmAgent):
+    # smaller agents in parallel
+
+    project_name_agent = LlmAgent(
+        name="project_name_agent",
+        model=LiteLlm(model="ollama_chat/gemma3:1b"),
+        instruction="""Return ONLY the name of the project as text in your response and NOTHING ELSE.""",
+        output_key="project_name",
+    )
+
+    project_description_agent = LlmAgent(
+        name="project_description_agent",
+        model=LiteLlm(model="ollama_chat/gemma3:1b"),
+        instruction="""Return ONLY the description of the project in your response and NOTHING ELSE.""",
+        output_key="project_description",
+    )
+
+    project_duration_agent = LlmAgent(
+        name="project_duration_agent",
+        model=LiteLlm(model="ollama_chat/gemma3:1b"),
+        instruction="""Return ONLY the duration of the project in minutes as an integer and NOTHING ELSE.""",
+        output_key="project_duration",
+    )
+
+    project_instructions_agent = LlmAgent(
+        name="project_instructions_agent",
+        model=LiteLlm(model="ollama_chat/gemma3:1b"),
+        instruction="""Return ONLY the step by step instructions of the project as a single string and NOTHING ELSE.""",
+        output_key="project_instructions",
+    )
+
+    project_materials_agent = LlmAgent(
+        name="project_materials_agent",
+        model=LiteLlm(model="ollama_chat/gemma3:1b"),
+        instruction="""Return ONLY the materials required for the project as text in your response and NOTHING ELSE. """,
+        output_key="project_materials",
+    )
+    return (
+        project_description_agent,
+        project_duration_agent,
+        project_instructions_agent,
+        project_materials_agent,
+        project_name_agent,
+    )
+
+
+@app.cell
+def _(
+    ParallelAgent,
+    project_description_agent,
+    project_duration_agent,
+    project_instructions_agent,
+    project_materials_agent,
+    project_name_agent,
+):
+    parallel_parser = ParallelAgent(
+        name="parallel_project_parser",
+        sub_agents=[
+            project_name_agent,
+            project_description_agent,
+            project_duration_agent,
+            project_instructions_agent,
+            project_materials_agent,
+        ],
+    )
+    return (parallel_parser,)
+
+
+@app.cell
+async def _(InMemoryRunner, parallel_parser):
+    _runner = InMemoryRunner(agent=parallel_parser)
+    await _runner.run_debug(
+        """
+        You are given this this example project:
+
+        Name: Toddler Art Project
+        description: A simple art project for toddlers using basic materials.
+        duration (in minutes): 30
+        instructions: 1. Gather all materials. 2. Provide paper and crayons to the toddler. 
+            3. Encourage the toddler to draw freely. 4. Display the artwork proudly.
+        materials:
+           2 Sheets of paper, crayons (assorted colors), glue stick, safety scissors. 
+
+        Convert this information into the ProjectBase and MaterialBase classes, and 
+        return a ProjectPackage object containing the project and its materials.
+
+        """
+    )
     return
 
 
